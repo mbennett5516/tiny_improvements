01b8896754910da7671138a66ab6bee727594593
mbennett <mbennett5516@yahoo.com>
2018-11-12 20:38:08 -0600

    server running

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require('express')
-const path = require('path');
-const Mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost/noteAppp', {useNewUrlParser: true});
+
+require('./routes/api-routes')(app);
+require('./routes/html-routes')(app);
+
+app.listen(PORT, function() {
+    console.log('App running on port ' + PORT);
+});
